02abbb9f874e40ae2b092eec17da7ecdf30583ad
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-11-10 19:31:41 -0400

    Aula 7 assistida

--- aula7/index.js
+++ aula7/index.js
@@ -1,19 +1,21 @@
-// constantes com constante
-
+// constantes
 // Nao podemos criar constantes com palavras reservadas
 // constantes precisam ter um nome significativo
-
-
-
-// Nao pode começar com numero
-
-// Nao podem conter espaços ou  traços
-
+// Nao pode comecar com numero
+// Nao podem conter espacos ou  tracos
 // utilizamos camelCase exemplo:
 // Case-sensitive
-// Nao podemos redeclarar constantes com let
-// NAO UTILIZE VAR, UTILIZE CONST
-const nome = 'Vinicius'
+// Nao podemos modificar o valor de uma constante
+// NAO UTILIZE VAR, UTILIZE CONST.
+const primeiro = 5;
+const segundo = 5;
+const resultado = primeiro*segundo
+const resultado2 = resultado * 2
+let resultado3plicado = resultado * 3
+console.log(resultado)
+console.log(typeof(primeiro)) // typeof() para saber o tipo de uma variavel.
+console.log(primeiro+segundo)
+
 
 
 
