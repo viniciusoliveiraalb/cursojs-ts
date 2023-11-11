a11d464ce497cc4a24e9a1f75c680dc02fdafdb4
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-11-11 18:36:21 -0400

    aula 10 - Operadores aritmeticos, atribui e increm

---
+++ aula10/index.js
@@ -0,0 +1,38 @@
+// Operadores aritmeticos, de atribuição e de incremento
+
+/* Aritméticos 
+
++ Adição e concatenação
+- Subtração
+/ Divisão
+* Multiplicação
+** potenciação 
+% resto da divisao
+
+*/
+
+/* ordem de precedencia
+
+ 1ª ()
+ 2ª **
+ 3º * / %
+ 4ª + - 
+
+ */
+
+ // ++ operador de incremento 
+ // -- operador de decremento
+
+ // atribuição 
+ // +=
+ // -=
+ // *= 
+ // **=
+ // ...
+
+
+
+const num1 = 10;
+const num2 = parseInt('3');
+console.log(num1%num2);
+console.log(typeof num2);
