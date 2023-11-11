7073d0911a88360b7c3a42ca34467c53ab3a65a3
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-11-11 17:58:16 -0400

    Aula 9.1 - Tipos de dados primitivos

---
+++ aula9/index1.js
@@ -0,0 +1,17 @@
+// String, Number, undefined, null, boolean e 
+const nome = 'Vini'; // string
+const nome1 = "Vini"; // string
+const nome2 = `Vini`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> nao aponta para nenhum local  na memoria
+const sobrenomeAluno = null; // Nulo nao aponta para nenhum local  na memoria
+const aprovado = true; // booleano = true ou falso logico
+
+let a = 2;
+const b = a;
+
+console.log(a, b);
+
+a = 3;
+console.log(a, b)
