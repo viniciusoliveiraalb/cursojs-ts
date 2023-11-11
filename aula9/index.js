68ae9e8570f1128c282c4cf5a34f19986471d683
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-11-11 17:39:13 -0400

    Aula 9 - Let vs Var

---
+++ aula9/index.js
@@ -0,0 +1,4 @@
+// Let vs Var - Diferenças
+var nome = 'Vinicius'; // primeira declaração
+var nome = 'OutroNome'; // segunda declaração
+console.log(nome) // var consigo redeclarar a variavel, ja com let e const nao consigo.
