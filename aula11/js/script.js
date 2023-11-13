bc9d5c274d53c2f9a75d741d6361f59ec3ff4f92
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-11-12 21:22:03 -0400

    Aula 11 - alert confirm e prompt

---
+++ aula11/js/script.js
@@ -0,0 +1,8 @@
+// aula 11 - alert, confirm e prompt
+
+let num1 = prompt('Digite um numero');
+let num2 = prompt('Digite outro numero');
+num1 = parseFloat(num1)
+num2 = parseFloat(num2)
+const resultado = num1+num2
+alert(`o resultado da soma é ${resultado}`)
