caa3d6e404d918343cecef2f526cbe453a211507
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-11-20 23:09:43 -0400

    Aula 16 - Arrays

---
+++ aula16/index.js
@@ -0,0 +1,27 @@
+// Arrays
+
+const alunos = ['Vinicius', 'Alicia', 'Fulano'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array) // verifica se é um array. 
+
+
+
+
+//alunos.push('Mais1')
+//alunos.push('Mais2')
+
+
+
+
+
+
+//console.log(alunos.slice(0, -1)); // para fatiar um array
+
+// delete alunos[1]; //deleta um conteudo de um elemento, nao altera os indices.
+//alunos.shift(); // adiciona um elemento no começo do array
+// alunos.pop(); // remove o ultimo elemento.
+//alunos.unshift('Siclano'); // adiciona o elemento no começa do array (ordem dos arrays muda automaticamente)
+//alunos[3] = 'Luiza';
+// alunos.push('Oliveira'); // Adiciona no fim
+//alunos[alunos.length] = 'Beutrano'; // alterar o indice existente : no caso o indice 0 pois ja existe valor
