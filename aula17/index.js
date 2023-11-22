b86a48d3a80b0f6b18611edb781f2ca8436f34e2
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-11-21 23:54:39 -0400

    Aula 17 - Function

---
+++ aula17/index.js
@@ -0,0 +1,51 @@
+// arrow function exemplo:
+
+const raiz = (n) => n** 0.5;
+
+
+/*
+
+const raiz = function(n){
+    return n**0.5;
+};
+
+*/
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+
+
+
+/*
+
+
+function soma(x, y){
+    const result = x+y;
+    return result;
+}
+
+console.log(soma(2, 2))
+
+*/
+
+//console.log(soma(2, 4))
+
+
+
+
+
+/*
+
+function  saudacao(nome){
+    
+    return `Bom dia! ${nome}!`;
+}
+
+const variavel = saudacao('Vinicius');
+console.log(variavel)
+
+*/
