f23c96e7e9c8624f038847e52caa1159ac0a2cd1
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-12-10 00:55:12 -0400

    Aula22 - Operadores Logicos

---
+++ aula22/index.js
@@ -0,0 +1,23 @@
+/* Operadores logicos
+
+&& -> AND -> E
+|| -> OR -> OU 
+! -> NOT -> NAO
+
+*/
+
+// const expressaoAnd = true && true && true;
+// const expressaoOr = true || false;
+// const expressaoNot = true ! true;
+
+// console.log(expressaoOr)
+
+// const usuario = 'Vinicius';
+// const senha = 12345;
+
+// const validarUser = usuario === 'Vinicius' && senha === 123456;
+
+// console.log(validarUser)
+
+console.log(!true)
+
