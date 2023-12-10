8036a73df69e0f23d6ce55a4cce9ec0115c22f69
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-12-10 00:21:18 -0400

    Corrigindo exemplos de aula21

--- aula21/index.js
+++ aula21/index.js
@@ -1,3 +1,18 @@
 /* operadores de comparação
 
-> 
+> maior que
+>= maior ou igual 
+< menor
+<= menor ou igual
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente valor e tipo
+
+*/
+
+const num1 = 10;
+const num2 = '10';
+const comparacao = num1 === num2;
+
+console.log(comparacao)
