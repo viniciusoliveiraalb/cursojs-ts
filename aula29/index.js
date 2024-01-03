dd241028e486af03bc9f39dc82ea2a7c2c8214f2
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2024-01-02 20:11:46 -0400

    Aula29 Switch/Case

---
+++ aula29/index.js
@@ -0,0 +1,47 @@
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
+
+}
+
+const data = new Date();
+const diaSemana = data.getDay();
+const diaSemanaTexto  = getDiaSemanaTexto(diaSemana);
+
+
+
+
+
+
+// if(diaSemana === 0){
+//     diaSemanaTexto = 'Domingo';
+// }else if(diaSemana === 0){
+//     // resultado
+// }
+
+console.log(diaSemana, diaSemanaTexto)
