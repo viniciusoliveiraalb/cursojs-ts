9b1a324aa7ae21622a8dccdef9b602443405dd02
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2024-01-13 15:11:05 -0400

    aula31 - +dif entre var, let e const

---
+++ aula31/index.js
@@ -0,0 +1,32 @@
+const verdadeira = true;
+
+// let tem escopo de bloco {..bloco}
+// var só tem escopo de função
+
+
+// let nome = 'vini'; // criando
+// var nome2 = 'vini';
+
+
+// if (verdadeira){
+//     let nome = 'oliver'; 
+//     var nome2 = 'oliver2'; // redeclarada
+// }
+//     if(verdadeira){
+//         var nome2 = 'w';
+//         let nome = 'outra coisa';
+       
+
+
+//     };
+
+//     console.log (nome, nome2)
+
+
+
+function falaOi(){
+    let nome = 'vinicius'
+    var sobrenome = 'oliveira';
+};
+    console.log(sobrenome);
+
