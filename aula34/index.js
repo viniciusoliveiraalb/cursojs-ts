c3fafcab2ff7490252ef288400b886ab5a003161
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2024-01-14 11:13:52 -0400

    aula34 for - estrutura de repetição

---
+++ aula34/index.js
@@ -0,0 +1,18 @@
+//  // for - estrutura de repetição.
+
+// // console.log('1')
+// // console.log('1')
+// // console.log('1')
+// // console.log('1')
+// // console.log('1')
+
+// // i - index 
+// for (let i = 0; i <= 5; i++){
+//     console.log(`Passo ${i}`)
+// } // nao tem ; no final. 
+
+const frutas = ['Maça', 'Abacaxi', 'Açaí'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
