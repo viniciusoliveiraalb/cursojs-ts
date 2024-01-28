03479f41f8dc8686c54b51b0e515142d4b3f0ec9
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2024-01-28 10:13:19 -0400

    aula37 - for of

---
+++ aula37/index.js
@@ -0,0 +1,30 @@
+
+const pessoa = {
+    nome: 'Vinicius',
+    sobrenome: 'Oliveira'
+};
+
+// For classico - geralmente com iteraveis (array ou string)
+// For in - Retorna o indice ou chave (string, arrays ou objetos)
+// For of - Retorna o valor em si (iteraveis, arrays ou string)
+
+
+// for (let i = 0; i < nome.length; i++){
+//     console.log([i])
+// }
+
+//------------------------------------------
+
+// for (let i in nome){
+//     console.log(nome[i]);
+// }
+
+//------------------------------------------
+
+// for (let valor of nome){
+//     console.log(valor);
+// }
+
+// nome.forEach(function(valor, indice, array){
+//     console.log(valor, indice, array);
+// })
